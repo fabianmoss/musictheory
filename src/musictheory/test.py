0cff1ab4a3de02fb8af91ff66e65706cec2ff1b2
Fabian C. Moss <fabianmoss@gmail.com>
2020-05-24 15:00:53 +0200

    rebuild

--- src/musictheory/test.py
+++ src/musictheory/test.py
@@ -1,15 +1,19 @@
-from main import Tone, Interval
+from musictheory import Tone, Interval
 import numpy as np
 
 if __name__ == "__main__":
     s = Tone(0,-1,-1) # C_0
     t = Tone(0,2,1) # F#'0
     c = Tone(0,0,0)
-    i = Interval(source=s,target=t)
+    i = Interval(source=c,target=s)
+    j = Interval(source=s, target=t)
+    
+    t + i
 
-    print(s.label, t.label)
-    print(i.interval)
-    print(i.get_specific_interval(octaves=True))
+
+    # print(s.label, t.label)
+    # print(i.interval)
+    # print(i.get_specific_interval(octaves=True))
 
     # print(t.pitch_class_chromatic)
     # print(s.pitch_class_fifths)
